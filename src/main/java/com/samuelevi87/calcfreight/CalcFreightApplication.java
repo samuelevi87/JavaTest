f6d0bd94e345f22d2486f54716d091697326c736
samuelevi87 <samuel.levi.alves@outlook.com>
2022-02-04 15:23:12 -0300

    Project created

---
+++ src/main/java/com/samuelevi87/calcfreight/CalcFreightApplication.java
@@ -0,0 +1,13 @@
+package com.samuelevi87.calcfreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcFreightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcFreightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samuelevi87/calcfreight/CalcFreightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelevi87.calcfreight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcFreightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
